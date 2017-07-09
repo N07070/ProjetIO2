4b3d63a48d967556905dc8dad0569b5e33814a6e
N07070 <you@example.com>
2017-07-09 16:10:52 +0200

    Updated the database structure

--- CREATE_DATABASE.sql
+++ CREATE_DATABASE.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4
--- https://www.phpmyadmin.net/
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: May 14, 2017 at 05:29 PM
--- Server version: 5.7.18-0ubuntu0.17.04.1
--- PHP Version: 7.0.15-1ubuntu4
+-- Host: localhost
+-- Generation Time: Jul 09, 2017 at 04:08 PM
+-- Server version: 5.5.55-0+deb8u1
+-- PHP Version: 5.6.30-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +14,13 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `projetIO2`
 --
+CREATE DATABASE IF NOT EXISTS `projetIO2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `projetIO2`;
 
 -- --------------------------------------------------------
 
@@ -27,14 +29,28 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `commentaires` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `uuid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `project` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `commentaire` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `user` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `date_send` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `date_send` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+`id` int(11) NOT NULL,
+  `uuid` text COLLATE utf8mb4_bin NOT NULL,
+  `name` text COLLATE utf8mb4_bin NOT NULL,
+  `users` text COLLATE utf8mb4_bin NOT NULL,
+  `options` text COLLATE utf8mb4_bin NOT NULL,
+  `creation_date` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -43,13 +59,13 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
 --
 
 CREATE TABLE IF NOT EXISTS `messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `user_from` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `user_to` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `group_to` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `date_send` datetime NOT NULL,
   `text_message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `seen_by` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -58,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
 --
 
 CREATE TABLE IF NOT EXISTS `projets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `uuid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `owner` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `nbr_upvote` bigint(20) NOT NULL,
@@ -73,8 +89,8 @@ CREATE TABLE IF NOT EXISTS `projets` (
   `pictures` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `resume` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `linked_group` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -83,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `projets` (
 --
 
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `uuid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `username` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `email` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -94,10 +110,72 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `is_premium` int(11) NOT NULL,
   `upvoted_projects` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `downvoted_projects` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `downvoted_projects` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `commentaires`
+--
+ALTER TABLE `commentaires`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `projets`
+--
+ALTER TABLE `projets`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `commentaires`
+--
+ALTER TABLE `commentaires`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `projets`
+--
+ALTER TABLE `projets`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
